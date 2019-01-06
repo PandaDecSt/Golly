ad19413e5395d43bd8df6efb514a41eb2e4065cd
Dobmod <1692898084@qq.com>
2019-01-06 12:59:57 +0800

    Initial commit

---
+++ app/src/main/java/com/dobando/golly/MainActivity.java
@@ -0,0 +1,14 @@
+package com.dobando.golly;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
